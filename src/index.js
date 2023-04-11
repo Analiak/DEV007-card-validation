83d53f10c82aa511471de1d404043fa3f66ee8d6
Analía Klein <analiaklein@gmail.com>
2023-04-11 09:30:20 -0400

    agrego input, boton validar y listener al boton

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
 
 console.log(validator);
+let button= document.getElementById("validar");
+button.addEventListener("click" , function () {
+    const numeroTarjeta = document.getElementById("numero-tarjeta").value;
+    alert(numeroTarjeta);
+});
+
+//const string = "4,3,4,9,5,6,5,2,0,2,5,8,0,8,9,4";
+//return '434'.charAt(1);
+
+ 
